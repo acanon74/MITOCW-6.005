876c8f59b61efcdcc6f7927e994e6577d764199c
acanon74 <69167857+Pixel74@users.noreply.github.com>
2023-07-16 01:23:58 +0500

    PS1 - Fourth problem completed

--- ProblemSet1-PiDigits/src/piwords/WordFinder.java
+++ ProblemSet1-PiDigits/src/piwords/WordFinder.java
@@ -17,9 +17,43 @@ public class WordFinder {
      *                mutated.
      * @return The list of needles that were found in the haystack.
      */
+
     public static Map<String, Integer> getSubstrings(String haystack,
                                                      String[] needles) { 
         // TODO: Implement (Problem 4.b)
-        return new HashMap<String, Integer>();
+        HashMap<String, Integer> result = new HashMap<>();
+
+        for (String w : needles) {
+
+            for (int i = 0; i < haystack.length(); i++) {
+
+                String subHaystack = haystack.substring(i);
+
+                if (searchWord(subHaystack, w)) {
+                    result.put(w, i);
+                    break;
+                }
+
+            }
+        }
+
+
+        return result;
     }
+
+    private static boolean searchWord(String text, String word) {
+
+        if (word.length() == 0) {
+            return true;
+        } else if (text.length() == 0 && word.length() != 0) {
+            return false;
+        } else if (text.charAt(0) == word.charAt(0)) {
+            return searchWord(text.substring(1),
+                    word.substring(1));
+        } else {
+            return false;
+        }
+
+    }
+
 }
--- ProblemSet1-PiDigits/src/piwords/WordFinderTest.java
+++ ProblemSet1-PiDigits/src/piwords/WordFinderTest.java
@@ -9,6 +9,48 @@ import org.junit.Test;
 
 public class WordFinderTest {
     @Test
+    /**
+     *
+     * Special cases:
+     *
+     * haystack.length == 0
+     * needles.length == 0
+     *
+     * Test strategy:
+     *
+     * We define a haystack and needles in which we can find all the possible
+     * outcomes for a given needle. Those being:
+     *
+     * - It is a word.
+     * - It is part of a word.
+     * - It is not in the haystack.
+     * - The needle can be found in two distinct places.
+     *
+     * Breadkdown of every test used:
+     *
+     * It is a word.
+     * expectedOutput2.put("alphabet", 0);
+     * expectedOutput2.put("generation", 9);
+     *
+     * It is part of a word.
+     * expectedOutput2.put("alpha", 0);
+     * expectedOutput2.put("bet", 5);
+     *
+     * It appears twice or more in the haystack
+     * expectedOutput2.put("alpha", 0); //The index must be 0 rather than 20.
+     *
+     * It is not in the haystack.
+     * needle2[n] == "software".
+     *
+     * haystack.length == 0.
+     * assertEquals(new HashMap<String, Integer>(), WordFinder.getSubstrings(
+     * new String(""), new String[] {"a", "b", "c"}));
+     *
+     * needles.length == 0
+     * assertEquals(new HashMap<String, Integer>(), WordFinder.getSubstrings(
+     * new String("abc"), new String[] {}));
+     */
+
     public void basicGetSubstringsTest() {
         String haystack = "abcde";
         String[] needles = {"ab", "abc", "de", "fg"};
@@ -18,9 +60,26 @@ public class WordFinderTest {
         expectedOutput.put("abc", 0);
         expectedOutput.put("de", 3);
 
-        assertEquals(expectedOutput, WordFinder.getSubstrings(haystack,
-                                                              needles));
-    }
+        assertEquals(expectedOutput, WordFinder.getSubstrings(haystack, needles));
 
-    // TODO: Write more tests (Problem 4.a)
+        //My solution tests:
+        String haystack2 = "alphabet generation alpha";
+        String[] needles2 = {"alphabet", "generation",
+                "alpha", "bet", "software"};
+
+        Map<String, Integer> expectedOutput2 = new HashMap<>();
+        expectedOutput2.put("alphabet", 0);
+        expectedOutput2.put("alpha", 0);
+        expectedOutput2.put("bet", 5);
+        expectedOutput2.put("generation", 9);
+
+        assertEquals(expectedOutput2, WordFinder.getSubstrings(haystack2,
+                needles2));
+
+        assertEquals(new HashMap<String, Integer>(), WordFinder.getSubstrings(
+                new String(""), new String[] {"a", "b", "c"}));
+
+        assertEquals(new HashMap<String, Integer>(), WordFinder.getSubstrings(
+                new String("abc"), new String[] {}));
+    }
 }
